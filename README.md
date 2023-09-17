da6fd3c33633a5c9f26384657bc683997bf6ff65
Krisiries <640422+Krisiries@users.noreply.github.com>
2023-09-17 15:49:12 -0700

    Initial Push - Affinity Mode Only

--- README.md
+++ README.md
@@ -1,2 +1,45 @@
-# StarfieldLauncher
-Streamer utility - Automatically adjusts the affinity of starfield to free system resources
+# Starfield Launcher
+Simple PowerShell-based helper program to disallow starfield from using every CPU core.
+
+As with all my scripts, this is provided AS-IS with no plans for future additions.
+
+
+# First Time Setup
+<ol>
+  <li>Download the PowerShell or precompiled exe</li>
+  <li>Optional - use PS2Exe to convert the script to an executable: https://github.com/MScholtes/PS2EXE</li>
+  <li>Run the script or exe the initial settings file <b>as administrator</b></li>
+  <li>If the script cannot find your starfield install, it will prompt you to move it to the install folder</li>
+  <li>If you wish to change from the default settings, open the csv created in notepad and change "111" as covered below in the example. <b>1 means Starfield cannot access a core, 0 means Starfield can</b></li>
+  <ol>
+    <li>Example:</li>
+    <li>Open task manager</li>
+    <li>Go to the Performance tab</li>
+    <li>Read the number of logical processors</li>
+    <li>I disable around 1/4 of them, for example if you have 16 logical processors, I disable 4</li>
+    <li>Type 1s and 0s where 1 disables a core, and 0 leaves it for starfield.</li>
+    <li>In the following example, I disable the first 4 of the 16 available logical processors</li>
+    <li>HIGH CORE NUMBER       LOW CORE NUMBER</li>
+    <li>"Affinity","0000000000001111"
+    <li>Any leading 0s can be dropped by default. Default settings will disable 3 cores</li>
+  </ol>
+</ol>
+
+# Subsequent Launch
+Run the script or program, and your programs will be started! Script will delay to show errors and confirmations.
+You can redirect the Starfield shortcut to the exe, if you do, the settings will be <i>in your starfield install folder</i>
+
+# FAQ
+## This launcher does...:
+<ul>
+  <li>searches the registry and its own folder for starfield's executable</li>
+  <li>reduces the number of logical cores starfield can use</li>
+  <li>help fix system stuttering with starfield in certain scenarios</li>
+  <li>allow customization for other games, no support will be provided</li>
+</ul>
+## This launcher does not...:
+<ul>
+  <li>change anything about starfield's own code</li>
+  <li>fix starfield's priority issues on alt tab</li>
+  <li>provide a magic fix for hardware below program spec</li>
+</ul>
---
+++ Starfield Launcher.ps1
@@ -0,0 +1,98 @@
+﻿if (-not (Test-Path -Path '.\SLSettings.csv')) {
+    Write-Output "SLSettings.csv not found. Writing settings file and defaulting to initial settings"
+
+    #Contents of sample settings file.
+    $sample = @(
+        [pscustomobject]@{Setting="Game"; Value="Starfield"},
+        [pscustomobject]@{Setting="Affinity"; Value="111"}
+    )
+    
+    #Write out sample, opens location where example was written   
+    $sample | Export-CSV -Path .\SLSettings.csv -NoTypeInformation
+    Write-Output "SLSettings.csv created"
+    $settings = $sample
+
+} else {
+    Write-Output "Settings file found, continuing exection"
+    $settings = Import-CSV .\SLSettings.csv
+}
+
+
+#Baseline Values
+$gameName = $settings[0].Value
+$affOffset = [Convert]::ToInt32($settings[1].Value,2)
+
+
+$method = 0
+if (Test-Path ".\$gameName.exe"){
+    $curLoc = ""
+    $curName = $gameName
+    $method = 3
+} else {
+    $game = Get-ChildItem HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Uninstall\* | % { Get-ItemProperty $_.PsPath } | Select DisplayName,InstallLocation | Sort-Object Displayname -Descending | Where Displayname -eq $gameName
+    $method = 1
+    if ($game -eq $null){
+        $game = Get-ChildItem HKLM:\Software\Wow6432Node\Microsoft\Windows\CurrentVersion\Uninstall\* | % { Get-ItemProperty $_.PsPath } | Select DisplayName,InstallLocation | Sort-Object Displayname -Descending | Where Displayname -eq $gameName
+        $method = 2
+    }
+    $curLoc = $game.InstallLocation
+    $curName = $game.DisplayName
+}
+
+switch ($method) {
+    0 {
+        Write-Output "$gameName not found, please move to install directory"
+        Pause
+        Exit
+    }
+    1 {Write-Output "$gameName found in Uninstall registry using method 1, continuing..."}
+    2 {Write-Output "$gameName found in Uninstall registry using method 2, continuing..."}
+    3 {Write-Output "$gameName found in current directory, continuing..."}
+}
+
+$curExe = $curName + ".exe"
+
+Write-Output "$curName found at $curLoc"
+
+cd $curLoc
+$test = Start-Process .\$curExe -PassThru
+
+while( (Get-WmiObject Win32_process -Filter "name = '$curExe'") -eq $null ) {
+  Write-Output "Sleeping while Starfield Starts"
+  Start-Sleep -s 10
+  cls
+}
+
+Write-Output "Starfield started, safety sleep for 10s"
+Start-Sleep -s 10
+
+$count = 0
+$runProc = Get-Process $curName
+
+$befAffinity = $runProc.ProcessorAffinity
+$aftAffinity = $befAffinity
+
+While ($befAffinity -ne ($aftAffinity + $affOffset)) {
+    Write-Output "Reducing affinity by '$affOffset'"
+    $runProc.ProcessorAffinity=($runProc.ProcessorAffinity - $affOffset)
+
+    Start-Sleep -s 5
+    $runProc = Get-Process $curName
+    $aftAffinity = $runProc.ProcessorAffinity
+    $count ++
+}
+
+if ($count > 4){
+    Write-Output "Affinity set failed, check administrator mode or try again"
+} else if ($count = 0) {
+	Write-Output "Affinity not changed"
+} else {
+    Write-Output "Affinity set correctly"
+}
+
+Start-Sleep -s 5
+
+Write-Output "Success! closing in 10s"
+Start-Sleep -s 10
+
+exit
